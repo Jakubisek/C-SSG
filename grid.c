c65875812f6b1335a10b502261921c5dfff6c729
Jakubisek <jakubisek555@seznam.cz>
2023-08-02 19:23:41 +0200

    tile update does not need to load neighbours into an array

--- grid.c
+++ grid.c
@@ -3,31 +3,17 @@
 
 
 void get_square(grid_t grid, tile_t *part, size_t index) {
-    size_t i = 0;
-    size_t counter = 0;
-    while (i < 9 && counter < 8) {
+    for (size_t i = 0; i < 9; i++) {
         size_t pos = 27*(index / 3) + 3*(index % 3) + (i % 3) + 9*(i / 3);
-        if (pos == index) {
-            i++;
-            continue;
-        }
-        part[counter++] = grid[pos];
-        i++;
+        part[i] = grid[pos];
     }
 }
 
 void get_line(grid_t grid, tile_t *part, size_t index, bool is_row) {
     size_t const start = is_row ? (index % 9) : index - (index % 9);
-    size_t counter = 0;
-    size_t i = 0;
-    while (i < 9 && counter < 8) {
+    for (size_t i = 0; i < 9; i++) {
         size_t pos = is_row ? start + i : start + 9*i;
-        if (pos == index) {
-            i++;
-            continue;
-        }
-        part[counter++] = grid[pos];
-        i++;
+        part[i] = grid[pos];
     }
 }
 
@@ -42,30 +28,48 @@ int fill_grid(grid_t grid, char const *data)
     return 81;
 }
 
-bool update_position(grid_t grid, size_t index)
+bool update_position(grid_t grid, size_t pos)
 {
-    tile_t row[8];
-    tile_t column[8];
-    tile_t square[8];
+    if (tile_is_solved(grid[pos])) return false;
+    tile_t before_update = grid[pos];
 
-    get_line(grid, row, index, true);
-    get_line(grid, column, index, false);
-    get_square(grid, square, index);
+    size_t row_start = pos / 9;
+    for (size_t i = 0; i < 9; i++) {
+        size_t current_pos = row_start + i;
+        if (current_pos != pos && tile_is_solved(grid[current_pos])) {
+            remove_from_tile(&grid[pos], grid[current_pos]);
+        }
+    }
 
+    size_t column_start = pos % 9;
+    for (size_t i = 0; i < 9; i++) {
+        size_t current_pos = column_start + (9 * i);
+        if (current_pos != pos && tile_is_solved(grid[current_pos])) {
+            remove_from_tile(&grid[pos], grid[current_pos]);
+        }
+    }
 
-    return remove_all_solved(row, 8, &grid[index]) || remove_all_solved(column, 8, &grid[index]) || remove_all_solved(square, 8, &grid[index]);
+    // SQUARE UPDATE IS HARD
+    // size_t square_corner_pos = pos - (pos % 3) - (((pos / 9) % 3) * 9);
+    // for (size_t i = 0; i < 4; i++) {
+    //     remove_from_tile(&grid[pos], grid[square_corner_pos + (18 * (i / 2) + 2 * (i % 2))]);
+    // }
+
+    return before_update == grid[pos];
 }
+
+const char *const line = "+---+---+---+ +---+---+---+ +---+---+---+\n";
 
 void show_grid(grid_t grid)
 {
-    printf("+---+---+---+ +---+---+---+ +---+---+---+\n");
+    printf(line);
     for (size_t i = 0; i < 9; i++) {
         for (size_t j = 0; j < 9; j++) {
             printf("| %c ", tile_to_char(grid[9*i+j]));
             if (j % 3 == 2 && j != 8) printf("| ");
         }
-        printf("|\n+---+---+---+ +---+---+---+ +---+---+---+\n");
-        if (i % 3 == 2 && i != 8) printf("+---+---+---+ +---+---+---+ +---+---+---+\n");
+        printf("%s%s", "|\n", line);
+        if (i % 3 == 2 && i != 8) printf(line);
     }
     putchar('\n');
 }
