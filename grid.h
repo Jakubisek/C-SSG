66aca42d58032c6a27e9bca5b2de58054f5b917a
Jakubisek <jakubisek555@seznam.cz>
2023-08-04 14:22:47 +0200

    simple tile update should be done recursively

--- grid.h
+++ grid.h
@@ -7,7 +7,9 @@ typedef tile_t* grid_t;
 
 int fill_grid(grid_t grid, char const *data);
 
-bool update_position(grid_t grid, size_t index);
+bool update_grid(grid_t grid);
+
+bool update_all_unique(grid_t grid);
 
 void show_grid(grid_t grid);
 
