eb6eaa5dcb6066896e7c11db8435f73035937aee
Jakubisek <jakubisek555@seznam.cz>
2023-08-05 20:04:53 +0200

    reformated all function documentation

--- tile.h
+++ tile.h
@@ -11,7 +11,7 @@ typedef unsigned short tile_t;
 
 /*
     tile_t
-    acts like a set that can contain the values 1-9 and also contains information
+    Acts like a set that can contain the values 1-9 and also contains information
     on how many elements are stored in this set
 
     out of the 16 bits:
@@ -27,70 +27,100 @@ typedef unsigned short tile_t;
 #define TILE_GET_SUM &0xF000
 #define TILE_GET_SET &0x01FF
 
-/*
-    Converts char into tile_t
-    '0' is converted to TILE_EMPTY
-    '1' to '9' are converted to their respective solved tiles
-    otherwise 0 tile is returned
-*/
+
+/**
+ * Converts a char into a tile_t.
+ * '0' is converted to TILE_EMPTY.
+ * '1' to '9' are converted to their respective solved tiles.
+ * Otherwise, TILE_ERROR is returned.
+ *
+ * @param num the character to convert.
+ * @return the corresponding tile_t value (with sum always equal to either 0, 1, or 9).
+ */
 tile_t char_to_tile(char num);
 
-/*
-    Checks if a tile has only one option for a number,
-    this is done by checking if the sum == 1
-*/
+/**
+ * Checks if a tile has only one option for a number.
+ *
+ * @param tile the tile to check.
+ * @return true if the sum of tile is equal to 1, false otherwise.
+ * @warning only checks the sum bits and ignores the rest, this might return true for invalid tiles
+ */
 bool tile_is_solved(tile_t tile);
 
-/*
-    Checks if a tile contains `num`
-    @warning ingores the sum of both parameters
 
-    @returns true if `num` is a subset of `tile`, false otherwise
-*/
+/**
+ * Checks if a tile contains all the elements from another tile.
+ *
+ * @param tile the tile to check
+ * @param num the number to look for in the tile.
+ * @return true if `num` is a subset of `tile`, false otherwise.
+ */
 bool tile_has_num(tile_t tile, tile_t num);
 
-/*
-    Performs a set union and adds `tile_to_add` to `tile`
-    @returns false if operation does not change the value of `tile`, true otherwise
-    @attention recalculates sum after the operation ignoring the old values
-*/
+
+/**
+ * Performs a set union and addition of sum bits adding `tile_to_add` to `tile`.
+ *
+ * @param tile a pointer to the tile to update.
+ * @param tile_to_add the tile to add to the original (first) tile.
+ * @return true if the operation changes the value of `tile`, false otherwise.
+ * @warning ignores sum bits of both parameters recalcualting a new value after addition
+ */
 bool add_to_tile(tile_t *tile, tile_t tile_to_add);
 
-/*
-    Preforms a set subtraction removing `tile_to_remove` from `tile`
-    @returns false if operation does not change the value of `tile`, true otherwise
-    @attention recalculates sum after the operation ignoring the old values
-*/
+
+/**
+ * Performs a set subtraction, removing `tile_to_remove` from `tile`.
+ *
+ * @param tile a pointer to the tile to update.
+ * @param tile_to_remove the tile to remove from the original (first) tile.
+ * @return true if the operation changes the value of `tile`, false otherwise.
+ * @warning ignores sum bits of both parameters recalcualting a new value after subtraction
+ */
 bool remove_from_tile(tile_t *tile, tile_t tile_to_remove);
 
-/*
-    Updates `tile_to_update` by removing all the values from `tiles` which are solved
-    @returns false if operation does not change the value of `tile_to_update`, true otherwise
-    @attention since remove_from_tile() is used, sum might be updated during this operaiton
-    @warning including `tile_to_update` in `tiles` will cause this tile to be ignored only if they have the same adress
-*/
+
+/**
+ * Updates `tile_to_update` by removing all the values from `tiles` which are solved.
+ *
+ * @param tiles an array of tiles to compare with.
+ * @param count the number of tiles in the array.
+ * @param tile_to_update A pointer to the tile to update.
+ * @return true if the operation changes the value of `tile_to_update`, false otherwise.
+ * @note including a tile with the same address as `tile_to_update` in tiles will cause it to be ignored
+ */
 bool remove_all_solved(tile_t *tiles, size_t count, tile_t *tile_to_update);
 
-/*
-    Updates (solves) all the tiles from `tiles` that contain a unique value
-    @returns false if no tiles are updated, true otherwise
-    @attention if a situation occurs where multiple values are unique, the largest will be chosen
-*/
+
+/**
+ * Updates (only solves) all the tiles from `tiles` that contain a unique value in that array.
+ *
+ * @param tiles an array of tiles to check.
+ * @param count the number of tiles in the array.
+ * @return true if any tiles are updated, false otherwise.
+ * @note in a situation where multiple values in tile are unique, the highest will be chosen
+ */
 bool solve_if_unique(tile_t *tiles, size_t count);
 
-/*
-    An inverse function to char_to_tile()
-    @returns
-    '?' if the tile is not solved,
-    '!' if `tile` contains error,
-    '1' - '9' otherwise
-*/
+
+/**
+ * Converts a tile_t to a character.
+ *
+ * @param tile The tile to convert.
+ * @return The character representation of the tile.
+ * @note '!' means the tile is equal to TILE_ERROR or 0
+ * @note ' ' means the tile is equal to TILE_EMTPY or 0x91FF
+ * @note '?' means the tile is not solved but also not equal to TILE_EMPTY
+ * @note 'X' means the tile format was not recognized as valid
+ */
 char tile_to_char(tile_t tile);
 
-/*
-    More readable binary representation of a tile with sum in decimal
-    @note TEMP, syntax (dec)sum-(bin)elements
-*/
+
+/**
+ * TEMP - Prints a more readable binary representation of a tile to stdout
+ * @note syntax (dec)sum-(bin)elements
+ */
 void show_tile(tile_t tile);
 
 #endif // TILE_H
