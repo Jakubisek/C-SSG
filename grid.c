8d834ad6878f833d8d9234ef691bf87ff7a09b54
Jakubisek <jakubisek555@seznam.cz>
2023-08-09 12:41:10 +0200

    improved fill_grid() and recursive_grid_update()

--- grid.c
+++ grid.c
@@ -2,7 +2,8 @@
 #include "grid.h"
 
 
-void get_part(grid_t grid, tile_t **part, size_t index, enum PART_TYPE type) {
+void get_part(grid_t grid, tile_t **part, size_t index, enum PART_TYPE type)
+{
     if (type == PART_SQUARE) {
         for (size_t i = 0; i < 9; i++) {
             size_t pos = 27*(index / 3) + 3*(index % 3) + (i % 3) + 9*(i / 3);
@@ -17,7 +18,8 @@ void get_part(grid_t grid, tile_t **part, size_t index, enum PART_TYPE type) {
     }
 }
 
-size_t pos_from_part(enum PART_TYPE part_type, size_t index, size_t order) {
+size_t pos_from_part(enum PART_TYPE part_type, size_t index, size_t order)
+{
     if (part_type == PART_ROW) {
         return index - (index % 9) + order;
     }
@@ -30,19 +32,56 @@ size_t pos_from_part(enum PART_TYPE part_type, size_t index, size_t order) {
 
 int fill_grid(grid_t grid, char const *data)
 {
-    for (size_t i = 0; i < 81; i++) {
-        if (data[i] == '\0') {
-            return i;
+    
+    size_t char_counter = 0;
+    size_t zero_counter = 0;
+    size_t grid_index = 0;
+
+    char c = data[char_counter];
+    bool empty_counting_mode = false;
+
+    while (c != '\0') {
+        if (grid_index >= 81) {
+            fprintf(stderr, "Excess data - ignoring all from '%c' at %ld\n", c, char_counter);
+            break;
         }
-        grid[i] = char_to_tile(data[i]);
+        if ((c > '9' || c < '0') && c != '*' && c != '-') {
+            fprintf(stderr, "Encountred an unexpected character '%c' at %ld\n", c, char_counter);
+            fprintf(stderr, "The rest of the grid will be filled with empty tiles...\n");
+            break;
+        }
+        if (c == '*') {
+            if (empty_counting_mode) {
+                for (size_t i = 0; i < zero_counter; i++) {
+                    if ((grid_index + i) >= 81) continue;
+                    grid[grid_index + i] = TILE_EMPTY;
+                }
+                grid_index += zero_counter;
+                zero_counter = 0;
+            }
+            empty_counting_mode = !empty_counting_mode;
+            c = data[++char_counter];
+            continue;
+        }
+        if (empty_counting_mode) {
+            zero_counter = (zero_counter * 10) + (c - '0');
+            if (zero_counter > 81) zero_counter = 81;
+        } else {
+            grid[grid_index++] = char_to_tile(c);
+        }
+        c = data[++char_counter];
     }
-    return 81;
+    if (grid_index < 81) {
+        for (size_t i = 0; i < 81 - grid_index; i++) {
+            grid[grid_index + i] = TILE_EMPTY;
+        }
+    }
+    return grid_index;
 }
 
-/*
-    TODO: make this readable using pos_from_part()
-*/
-bool recursive_grid_update(grid_t grid, size_t pos) {
+
+bool recursive_grid_update(grid_t grid, size_t pos)
+{
     
     if (!tile_is_solved(grid[pos])) return false;
     bool result = false;
@@ -52,27 +91,16 @@ bool recursive_grid_update(grid_t grid, size_t pos) {
         show_tile(grid[pos]); putchar('\n');
     #endif
 
-    for (size_t is_row = 0; is_row < 2; is_row++) {
-        size_t start = is_row ? (pos / 9) * 9 : pos % 9;
+    for (int part_type = 0; part_type < 3; part_type++) {
+        size_t start = 
+            (part_type == PART_SQUARE) ? pos - (pos % 3) - (((pos / 9) % 3) * 9) :
+            (part_type == PART_ROW) ? ((pos / 9) * 9) : pos % 9;
+
         for (size_t i = 0; i < 9; i++) {
-            size_t current_pos = is_row ? start + i : start + (9 * i);
-            if (current_pos == pos || tile_is_solved(grid[current_pos])) {
-                continue;
-            }
-            if (remove_from_tile(&grid[current_pos], grid[pos]) && tile_is_solved(grid[current_pos])) {
-                result = true;
-                recursive_grid_update(grid, current_pos);
-            }
-        }
-    }
-    /*  
-        remove from the 8 square tiles out of which 4 have already been updated
-        checking which were and were not updated would be slower than just doing it twice
-    */
-    size_t square_corner_pos = pos - (pos % 3) - (((pos / 9) % 3) * 9);
-    for (size_t i = 0; i < 27; i += 9) {
-        for (size_t j = 0; j < 3; j++) {
-            size_t current_pos = square_corner_pos + i + j;
+            size_t current_pos = 
+                (part_type == PART_SQUARE) ? start + ((i / 3) * 9) + (i % 3) :
+                (part_type == PART_ROW) ? start + i : start + (9 * i);
+
             if (current_pos == pos || tile_is_solved(grid[current_pos])) {
                 continue;
             }
@@ -86,7 +114,8 @@ bool recursive_grid_update(grid_t grid, size_t pos) {
 }
 
 
-bool update_grid(grid_t grid) {
+bool update_grid(grid_t grid)
+{
     bool result = false;
     for (size_t i = 0; i < 81; i++) {
         if (tile_is_solved(grid[i])) {
@@ -103,11 +132,12 @@ bool update_all_unique(grid_t grid)
     for (size_t i = 0; i < 9; i++) {
         for (int part_type = 0; part_type < 3; part_type++) {
             get_part(grid, part, i, (enum PART_TYPE) part_type);
-            if (solve_if_unique(part, 9)) {
-                result = true;
-                for (size_t j = 0; j < 9; j++) {
-                    recursive_grid_update(grid, pos_from_part((enum PART_TYPE)part_type, i, j));
-                }
+            if (!solve_if_unique(part, 9)) {
+                continue;
+            }
+            result = true;
+            for (size_t j = 0; j < 9; j++) {
+                recursive_grid_update(grid, pos_from_part((enum PART_TYPE)part_type, i, j));
             }
         }
     }
