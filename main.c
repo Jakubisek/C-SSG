454ffaf7c0273247ae983509b5ededa7e17a0d04
Jakubisek <jakubisek555@seznam.cz>
2023-08-11 22:00:33 +0200

    added error detection message for unsolvable situations

--- main.c
+++ main.c
@@ -11,15 +11,20 @@ int main(int argc, char const *argv[])
 
     int cycle_count = 0;
     while((update_grid(grid) || update_all_unique(grid))) {
-        printf("Performed cycle %d\n", cycle_count++);
+        printf("Performed cycle %d", cycle_count++);
+
+        if (grid_contains_errors(grid)) {
+            printf(" - grid is unsolvable\n");
+        }
+
         show_grid(grid);
     }
     
     printf("---\nDone\n");
 
-    if (grid_is_solved(grid)) {
+    if (grid_has_only_solved(grid)) {
         printf("Solved all tiles ");
-        printf("%s\n", grid_is_correct(grid) ? "with no errors" : " - SOLUTION CONTAINS ERRORS");
+        printf("%s\n", grid_solved_correctly(grid) ? "with no errors" : " - SOLUTION CONTAINS ERRORS");
     } else {
         printf("but incomplete - not all tiles are solved\n");
     }
