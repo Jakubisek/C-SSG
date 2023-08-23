74a3aabd8e88a9e17b8881627dc1b06ab7b8181c
Jakubisek <jakubisek555@seznam.cz>
2023-08-23 16:37:36 +0200

    temporary - move messages into different file

--- main.c
+++ main.c
@@ -1,46 +1,23 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "grid.h"
+#include "solver.h"
 
 int main(int argc, char const *argv[])
 {
-    tile_t grid[81];
-    int filled = fill_grid(grid, argv[1]);
-    printf("---\nLoaded %d numbers into grid\n\n", filled);
-    show_grid(grid);
+    if (argc != 2) return EXIT_FAILURE;
 
-    update_grid(grid);
-
-    show_tile(grid[20]); putchar('\n');
-    
-    if (grid_contains_errors(grid)) {
-        fprintf(stderr, "Grid has no solutions\n---\nDone\n");
+    sstack_t *solving_stack = create_solving_stack(argv[1], 10);
+    if (solving_stack == NULL) {
+        fprintf(stderr, "[FATAL ERROR] Could not create solving stack\n");
         return EXIT_FAILURE;
     }
 
+    printf("Initial grid looks like this:\n");
+    show_grid(solving_stack->data_array[0]->game_data);
 
-    int cycle_count = 0;
-    while((update_grid(grid) || update_all_unique(grid))) {
-        printf("Performed cycle %d", cycle_count++);
+    printf("Solving...\n\n");
+    solve(solving_stack, 30);
 
-        if (grid_contains_errors(grid)) {
-            printf(" - grid is unsolvable\n");
-        } else putchar('\n');
-
-        show_grid(grid);
-    }
-    
-    printf("---\nDone\n");
-
-    if (grid_has_only_solved(grid)) {
-        printf("Solved all tiles ");
-        printf("%s\n", verify_solution(grid) ? "with no errors" : " - SOLUTION CONTAINS ERRORS");
-    } else {
-        printf("but incomplete - not all tiles are solved\n");
-    }
-    show_grid(grid);
-
+    terminate_solving(solving_stack, "No problems found during solving.\n", 0);
     return EXIT_SUCCESS;
 }
-
-// ./run 00608040000700108000045*4*3002051002*4*60905*6*400090020106*4*8003
