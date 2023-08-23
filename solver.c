9259fbb88c6972a7bdf83cd3675b1963b33964ba
Jakubisek <jakubisek555@seznam.cz>
2023-08-23 20:47:48 +0200

    changed info messages and re-enabled depth atribute

--- solver.c
+++ solver.c
@@ -4,13 +4,15 @@
 #define SOLVING_TERMINATED "[SOLVING TERMINATED] Additional information:\n%s"
 #define NUMBER_OF_SOLUTION_EXCEEDED "Number of solutions found exceeds the given limit - cannot continue solving.\n"
 #define DEBUGMSG_STACK_CREATION_OK "[INFO] Stack was successfully created and loaded with %d numbers.\n"
-#define WARNING_DEPTH_EXCEEDED "[WARNING] Discarded possible solution during fork - depth exceeded.\n"
+#define INFO_SOLUTION_FOUND "[INFO] Found and verified solution %d\n(required %ld additional numbers):\n"
+#define INFO_ALL_SOLUTIONS_LISTED "[INFO] All valid solutions were found - no other are possible\n"
+#define WARNING_STACK_NULL "[WARNING] Solution stack was incorrectly initalized and could not be freed.\n"
 #define FATAL_MAX_MEMORY_EXCEEDED "Solving was abruptly terminated - memory usage limit for stack exceeded.\n"
 #define FATAL_MALLOC_FAILED "Solving was abruptly terminated - malloc failed to allocate memory.\n"
 
 #define SSTACK_TOP solving_stack->item_count - 1
 
-sstack_t *create_solving_stack(char const *data, size_t initial_depth_limit)
+sstack_t *create_solving_stack(char const *data, size_t size_limit)
 {
     sstack_t *solving_stack = malloc(sizeof(sstack_t));
     if (solving_stack == NULL) return NULL;
@@ -21,18 +23,18 @@ sstack_t *create_solving_stack(char const *data, size_t initial_depth_limit)
         return NULL;
     }
 
-    solving_stack->max_capacity = INIT_SSTACK_SIZE;
     solving_stack->data_array[0] = malloc(sizeof(sgame_t));
     if (solving_stack->data_array[0] == NULL) {
         free(solving_stack->data_array);
         free(solving_stack);
         return NULL;
     }
+    solving_stack->size_limit = size_limit;
+    solving_stack->max_capacity = INIT_SSTACK_SIZE;
 
     int filled = fill_grid(solving_stack->data_array[0]->game_data, data);
-
-    // DEPTH IS DISABLED FOR NOW
-    //solving_stack->data_array[0]->depth = initial_depth_limit;
+    
+    solving_stack->data_array[0]->depth = 0;
     solving_stack->item_count = 1;
     #ifdef DEBUG_MSG
         printf(DEBUGMSG_STACK_CREATION_OK, filled); 
@@ -49,7 +51,7 @@ static void append_to_stack(sstack_t *solving_stack, sgame_t *game_to_append)
         solving_stack->data_array[solving_stack->item_count++] = game_to_append;
         return;
     }
-    if (solving_stack->max_capacity * 2 > (size_t)MAX_USED_MEMORY_LIMIT) {
+    if (solving_stack->max_capacity * 2 > (size_t)solving_stack->size_limit) {
         terminate_solving(solving_stack, FATAL_MAX_MEMORY_EXCEEDED, 1);
     }
     sgame_t **new_array = realloc(solving_stack->data_array, solving_stack->max_capacity * 2);
@@ -61,34 +63,22 @@ static void append_to_stack(sstack_t *solving_stack, sgame_t *game_to_append)
     solving_stack->max_capacity *= 2;
 }
 
-/*
-    DO NOT use this during fork, just reuse the existing game and append more
-*/
-static void pop_stack_top(sstack_t *solving_stack) {
+
+static void pop_stack_top(sstack_t *solving_stack)
+{
     if (solving_stack->item_count == 0) return;
     free(solving_stack->data_array[SSTACK_TOP]);
     solving_stack->data_array[--solving_stack->item_count] = NULL;
 }
 
 
-
-/*
-    This function should never receive a solved grid
-    doing that should cause segfault but might cause undefined behaviour 
-*/
 static void fork_stack_top(sstack_t *solving_stack)
 {
-    /* DEPTH DISABLED FOR NOW
-    if (solving_stack->data_array[SSTACK_TOP]->depth == 0) {
-        printf(WARNING_DEPTH_EXCEEDED);
-        pop_stack_top(solving_stack);
-        return;
-    }
-    solving_stack->data_array[SSTACK_TOP]->depth--;
-    */
+    
+    solving_stack->data_array[SSTACK_TOP]->depth++;
     grid_t grid = solving_stack->data_array[SSTACK_TOP]->game_data;
 
-    // assume there is at least on unsolved tile
+    // assume there is at least one unsolved tile
     tile_t candidate = 0xF000;
     size_t pos = __LONG_MAX__;
     for (size_t i = 0; i < 81; i++) {
@@ -119,11 +109,16 @@ int solve(sstack_t *solving_stack, size_t max_solutions_limit)
 {
     int solution_counter = 0;
 
+    // maybe add proper warnings for this
+    if (solving_stack == NULL || max_solutions_limit == 0) return 0; 
+
     update_grid(solving_stack->data_array[SSTACK_TOP]->game_data);
     if (grid_contains_errors(solving_stack->data_array[SSTACK_TOP]->game_data)) {
-        printf("The initial grid data contians a trivial mistake - grid has no solutions.\n");
-        printf("(re-run with debug messages to see details)\n\n");
-        return 0;
+        #ifndef DEBUG_MSG
+            printf("The initial grid data contians a trivial mistake - grid has no solutions.\n");
+            printf("(re-run with debug messages to see details)\n\n");
+            return 0;
+        #endif
     }
 
     while (solving_stack->item_count > 0) {
@@ -144,7 +139,7 @@ int solve(sstack_t *solving_stack, size_t max_solutions_limit)
             if (solution_counter == max_solutions_limit) {
                 terminate_solving(solving_stack, NUMBER_OF_SOLUTION_EXCEEDED , 1);
             }
-            printf("Found and verified solution %d:\n", solution_counter++);
+            printf(INFO_SOLUTION_FOUND, solution_counter++, solving_stack->data_array[SSTACK_TOP]->depth);
             show_grid(grid);
             pop_stack_top(solving_stack);
             continue;
@@ -153,18 +148,15 @@ int solve(sstack_t *solving_stack, size_t max_solutions_limit)
             fork_stack_top(solving_stack);
         }
     }
-
-    printf("Done solving - ");
-    if (solution_counter == 0) printf("The given grid has no valid solutions.\n");
-
     return solution_counter;
 }
 
 void terminate_solving(sstack_t *solving_stack, char const *message, int exit_status)
 {
+    if (solving_stack->item_count == 0) printf(INFO_ALL_SOLUTIONS_LISTED);
     printf(SOLVING_TERMINATED, message);
     if (solving_stack == NULL) {
-        // PRINT ERROR - could not set up solving stack
+        printf(WARNING_STACK_NULL);
         return;
     }
 
