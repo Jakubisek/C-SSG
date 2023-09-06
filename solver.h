3feb459291d887a5f4d0d0ea0fa68710be41639d
Jakubisek <jakubisek555@seznam.cz>
2023-09-06 13:36:40 +0200

    realloc for sstack_t is unnecessary - edited documentation

--- solver.h
+++ solver.h
@@ -1,7 +1,7 @@
 #include "grid.h"
 #include <stdlib.h>
 
-#define INIT_SSTACK_SIZE 100
+#define INIT_SSTACK_SIZE 729
 
 /*
     Solving strategy:
@@ -9,10 +9,9 @@
     if this doesn't solve the grid, take the first tile with the least amount of option for number it can contain
     and perform a "fork" - add all the variants of the game into the solving stack effectively solving that tile.
 
-    Next, take the top item of the stack and repeat this process until the stack is empty,
-    or some arbitrary number of items in the stack is exceeded. Whenever a grid is solved or it could
-    be determined that the partial soution is incorect, it should be immediately removed from the stack
-    (in case it is validated as correct, solution should be displayed).
+    Next, take the top item of the stack and repeat this process until the stack is empty.
+    Whenever a grid is solved or it could be determined that the partial soution is incorect, it should
+    be immediately removed from the stack (in case it is validated as correct, solution should be displayed).
 */
 
 
@@ -33,30 +32,30 @@ typedef struct solving_stack {
     sgame_t **data_array; // holds unsolved grids
     size_t item_count; // the number of items in data_array
     size_t max_capacity; // the actual allocated size of data_array
-    size_t size_limit; // stack size cannot exceed this value
 } sstack_t;
 
 /**
  * Initializes the solving stack through malloc() and in case of succeedes returns a pointer to it.
+ * The size of the stack is explicitly difined as 729 and cannot be exceeded, since that is
+ * the maximum number of unsolved games that might be theoretically processed at the same time,
+ * it is impossible to exceed this value.
  * 
  * @param data a string for the inital value in the stack (that can be parsed into grid_t)
- * @param size_limit the maximum size that can be allocated for the stack - realloc() will not exceed this value
  * @return A pointer to the solving stack or NULL if dynamic allocation failed.
- * @note A rough estimate of the maximum possible memory that might be used by the stack is:
- * @note memory in bytes = size_limit * (sizeof(sgame_t*) + sizeof(sgame_t))
- * @warning size_limit cannot be set smaller than INIT_SSTACK_SIZE, recomended value is at least double that amount
+ * @note INIT_SSTACK_SIZE = 729 because for every tile in grid (=81) thare can be at most 9 forks and 9*81 = 729,
+ * @note the actual size of the solving stack should be less then 6 KB when empty
  */
-sstack_t *create_solving_stack(char const *data, size_t size_limit);
+sstack_t *create_solving_stack(char const *data);
 
 
 /**
- * Solve the contents of the solving stack dyplaying all solutions found to stdout. If the number of solutions
+ * Solve the contents of the solving stack displaying all solutions found to stdout. If the number of solutions
  * found exceeds max_solution, this function will preemtively end solving (meaning it is possible that not all
  * solutions were found). If this function ecounter a fatal memory failure (malloc() fails, used memory exceeds
  * the set limit), the function will terminate by calling terminate_solving().
  * 
  * @param solving_stack a valid pointer to non-empty solving stack that can be resized by realloc()
- * @param max_solution_limit the number of found solution will not exceed this value
+ * @param max_solutions_limit the number of found solution will not exceed this value
  * @return The total number of solutions that were verified and displayed.
  */
 int solve(sstack_t *solving_stack, size_t max_solutions_limit);
